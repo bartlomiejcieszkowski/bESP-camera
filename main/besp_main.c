477bd58cdf994ed4652b6ab5eeebbb66e1502217
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2020-01-06 10:30:45 +0100

    and add simple console to main
    Signed-off-by: Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>

--- main/besp_main.c
+++ main/besp_main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include "micro_rtsp_server.h"
+#include "simple_console.h"
 
 static const char* TAG = "main";
+
+static const char* prompt = "besp> ";
+static const char* prompt_color = LOG_COLOR_I "besp> " LOG_RESET_COLOR;
 
 void app_main()
 {
     initialize_camera();
+    initialize_simple_console();
 
     register_micro_rtsp();
 
-    /* Main loop */
-    while(true) {
-    }
+    /* console task has a main loop */
+    simple_console_task(NULL);
 }
